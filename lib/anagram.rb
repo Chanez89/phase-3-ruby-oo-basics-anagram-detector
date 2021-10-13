031d88835a55eecc3ecd00c9cb2e28cb70756638
Neil B. Chanez Gracia <Chanez89@Hotmail.com>
2021-10-13 09:44:46 -0700

    commit tree b79764efdf45eca5e7f159508399dbf9ff693f4a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    
+     def initialize(word)
+        @word = word
+     end
+     
+     def match(words_arr)
+            #iterate through the words array
+            # if the word is an anagram of the initialized word, select it by putting it into the new array
+            # return that new array
+        words_arr.select do |single_word|
+                # split the two words into arrays and compare them
+            single_word.chars.sort == @word.chars.sort
+        end
+     end
+     
+end
+
+
+
+# listen = Anagram.new('listen')
+# listen.match(%w[enlists google inlets banana])
